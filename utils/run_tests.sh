c3e60f287a5f021987a209fd757342461fbef749
Piet Delport <piet@praekelt.com>
2012-01-25 12:36:41 -0500

    Show stats too.

--- utils/run_tests.sh
+++ utils/run_tests.sh
@@ -8,6 +8,7 @@ echo "=== Erasing previous coverage data..."
 coverage erase
 echo "=== Running trial tests..."
 coverage run `which trial` --reporter=subunit "${@:-vumi}" | tee results.txt | subunit2pyunit
+subunit-stats <results.txt
 subunit2junitxml <results.txt >test_results.xml
 rm results.txt
 echo "=== Processing coverage data..."
