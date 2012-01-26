537ccb93d9f38aeed573ac10cb0f4e99f3fe8c07
Piet Delport <piet@praekelt.com>
2012-01-26 09:06:33 -0500

    Support the Debian/Ubuntu package of coverage.py.

--- utils/run_tests.sh
+++ utils/run_tests.sh
@@ -1,18 +1,21 @@
 #!/bin/sh -e
 
+trial="$(which trail || exit)"
+coverage="$(which coverage || which python-coverage || exit)"
+
 cd "$(dirname "$0")/.."
 
 echo "=== Nuking old .pyc files..."
 find vumi/ -name '*.pyc' -delete
 echo "=== Erasing previous coverage data..."
-coverage erase
+python "$coverage" erase
 echo "=== Running trial tests..."
-coverage run `which trial` --reporter=subunit "${@:-vumi}" | tee results.txt | subunit2pyunit
+python "$coverage" run "$trial" --reporter=subunit "${@:-vumi}" | tee results.txt | subunit2pyunit
 subunit-stats <results.txt
 subunit2junitxml <results.txt >test_results.xml
 rm results.txt
 echo "=== Processing coverage data..."
-coverage xml
+python "$coverage" xml
 echo "=== Checking for PEP-8 violations..."
 pep8 --repeat --exclude='migrations' vumi | tee pep8.txt
 echo "=== Done."
